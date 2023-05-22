ca4065d5940915826babde332f38b96dd94adce8
austin.may1 <austin.may@protonmail.com>
2023-05-21 21:14:31 -0400

    Add project files.

---
+++ RanksBot/Commands.cs
@@ -0,0 +1,51 @@
+﻿using Discord;
+using Discord.Net;
+using Discord.WebSocket;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RanksBot
+{
+    public class Commands
+    {
+        private DiscordSocketClient _client = null!;
+
+        public Commands(DiscordSocketClient client)
+        {
+            _client = client;
+            _client.Ready += Client_Ready;
+        }
+
+        public async Task Client_Ready()
+        {
+            var guild = _client.GetGuild(1031999653094363177);
+
+            var guildCommand = new SlashCommandBuilder();
+
+            guildCommand.WithName("ping");
+            guildCommand.WithDescription("A game of ping-pong!");
+
+            var globalCommand = new SlashCommandBuilder();
+
+            globalCommand.WithName("hi");
+            globalCommand.WithDescription("Hello");
+
+            try
+            {
+                 await guild.CreateApplicationCommandAsync(guildCommand.Build());
+                 await guild.CreateApplicationCommandAsync(globalCommand.Build());
+            }
+            catch (HttpException e)
+            {
+                var json = JsonConvert.SerializeObject(e.Errors, Formatting.Indented);
+
+                Console.WriteLine(json);
+            }
+
+        }
+    }
+}
---
+++ RanksBot/Program.cs
@@ -0,0 +1,43 @@
+﻿using Discord;
+using Discord.WebSocket;
+using Newtonsoft.Json.Linq;
+
+namespace RanksBot
+{
+    public class Program
+    {
+        private DiscordSocketClient _client = null!;
+        private LoggingService _log = null!;
+        private Commands _commands = null!;
+        
+        public static Task Main(string[] args) => new Program().MainAsync();
+
+        private async Task MainAsync()
+        {
+            _client = new DiscordSocketClient();
+            _log = new LoggingService(_client);
+            _commands = new Commands(_client);
+
+            _client.SlashCommandExecuted += SlashCommandHandler;
+
+            var token = File.ReadAllText("token.txt");
+
+            await _client.LoginAsync(TokenType.Bot, token);
+            await _client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task SlashCommandHandler(SocketSlashCommand command)
+        {
+            if (command.Data.Name == "ping")
+            {
+                await command.RespondAsync($"You executed {command.Data.Name}\npong!");
+            }
+            else
+            {
+                await command.RespondAsync("Hello!");
+            }
+        }
+    }
+}
